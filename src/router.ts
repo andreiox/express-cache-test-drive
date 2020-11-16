faba28d3b6c02b481d36cba15f027ceee5308113
Andre Macedo <andre.macedo@seidor.com.br>
2020-11-16 10:30:53 -0300

    feat: post request with cache

--- src/router.ts
+++ src/router.ts
@@ -12,4 +12,12 @@ router.get('/', (req: Request, res: Response) => {
     res.status(200).json(response);
 });
 
+router.post('/', (req: Request, res: Response) => {
+    const response = { datetime: new Date().toISOString(), ...req.body };
+
+    set(res.locals.hash, JSON.stringify(response));
+
+    res.status(200).json(response);
+});
+
 export const mainRouter: Router = router;
